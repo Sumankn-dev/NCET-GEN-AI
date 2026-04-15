0a69f1975a585eb222436840a6a84251dc649921
Suman K N <sumankn26@outlook.com>
2026-04-15 10:57:23 +0530

    Update App.py

--- App.py
+++ App.py
@@ -3,4 +3,30 @@ from groq import Groq
 
 st.set_page_config("SumanAI Bot", layout="wide")
 st.title("SumanAI - Content Generator")
-st.image(
+st.image("IMG_1714.JPG")
+
+client = Groq(api_keys=st.secrets["GROQ_API_KEY"])
+#Get Product Name and Audience for that product
+product = st.text_input("Product")
+audience = st.text_input("Audience")
+#Button to Generate Content
+if st.button("Generate Content"):
+  prompt = f"Write marketing content for {product} targeting {audience}."
+  response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[{"role": "user","content": prompt}]
+  )
+  st.session_state.text = response.choices[0].message.content
+  st.write(text)
+  # After Content Create - Download The File
+if "text" in st.session_state:
+    content = st.text_area("Generated Content", st.session_state.text, height=300)
+    st.download_button(
+            label="⬇️ Download as TXT",
+            data=content,
+            file_name="marketing_copy.txt",
+            mime="text/plain"
+        )
+    else:
+        st.info("Generate content first")
+  
